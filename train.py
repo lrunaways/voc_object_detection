eae775903d186b4638422c89317618127283db6b
Nikita <n.plaksin8@gmail.com>
2021-09-28 02:31:57 +0300

    change dataset size

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from training.training_loop import training_loop
 
 
 if __name__ == '__main__':
-    train_dataloader = get_voc_dataloader(image_set='train', size=6, batch_size=2)
-    val_dataloader = get_voc_dataloader(image_set='val', size=2, batch_size=1)
+    train_dataloader = get_voc_dataloader(image_set='train', size=500, batch_size=2)
+    val_dataloader = get_voc_dataloader(image_set='val', size=200, batch_size=1)
 
     labels_map = train_dataloader.dataset.labels_map  # маппит имена в целые лейблы классов
     assert len(labels_map) == 20 + 1  # 20 класов + 'background'
